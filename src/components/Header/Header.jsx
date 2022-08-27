a614940e31d575bd1ee2858425c45ce37570fa0f
lisvega <lissyrivera456@gmail.com>
2022-08-27 15:56:14 +0200

    maquetación del Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,36 @@
+import "./Header.css"
+import { Link } from "react-router-dom";
+
+
 const Header = () => {
     return (
-        <div>Header</div>
+        <header>
+
+            <h1>Mis Mascotas</h1>
+            <nav>
+                <img src="https://images-ext-2.discordapp.net/external/Frz1bbdfy4ji9y7uwoYBtx7SwGUL5kPxPnM0Q7aly1U/%3Fw%3D2000/https/img.freepik.com/vector-premium/lindo-logo-tienda-mascotas-gato-perro_454510-133.jpg?width=702&height=702" alt="logo" />
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/mascotas">Mascotas</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    <li>
+                        <Link to="/register">Register</Link>
+                    </li>
+
+                </ul>
+
+
+            </nav>
+        </header>
+
+
+
     )
 }
 
