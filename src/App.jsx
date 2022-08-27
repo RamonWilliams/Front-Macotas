7aa7fc5f0e5b6f70364bbeff95fad5c7d71460c7
Ramon Williams <ramnwor21@gmail.com>
2022-08-27 13:19:34 +0200

    Mofificación del app

--- src/App.jsx
+++ src/App.jsx
@@ -10,28 +10,36 @@ import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 
 const App = () => {
-  return
-  <div>
-    <Router>
-      <Header />
-
-      <Routes>
-
-        <Route path="/" element={<Home />} />
-        <Route path="/mascotas" element={<Mascotas />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/newmascota" element={<NewMascota />} />
-        <Route path="/editmascota" element={<EditMascota />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+  return(
+   <div>
+     <Router>
+       <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mascotas" element={<Mascotas />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/newmascota" element={<NewMascota />} />
+          <Route path="/editmascota" element={<EditMascota />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
 
 
 
-      <Footer />
-    </Router>
+        </Routes>
+                
+       
+       <Footer/>
 
-  </div>;
+     </Router>
+
+   </div>
+
+
+  )
+ 
+  
+
+  
 
 };
 
