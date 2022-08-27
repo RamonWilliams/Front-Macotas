cb72681d90826e74da1fc260077b0cacfe2d9108
lisvega <lissyrivera456@gmail.com>
2022-08-27 19:05:02 +0200

    importación del useState y useEffect, ademas de hacer la petición a travez de nuestra carpeta de service API.

--- src/pages/Mascotas/Mascotas.jsx
+++ src/pages/Mascotas/Mascotas.jsx
@@ -1,9 +1,38 @@
+
+
+import React from "react";
+import { useState, useEffect } from "react";
+import { API } from "../../services/API";
+
+
 const Mascotas = () => {
+    const [allMascotas, setAllMascotas] = useState([]);
+
+    const getAllMascotas = async () => {
+        API.get("/mascotas").then((restMascotas) => {
+            setAllMascotas(restMascotas.data.mascotas)
+        });
+    };
+
+    useEffect(() => {
+        getAllMascotas();
+    }, []);
+
+
+
     return (
 
-        <div>
-            Mascotas
-        </div>
+        <section>
+            <h2>Mascotas</h2>
+
+            <div>
+
+                {allMascotas.length > 0 ? allMascotas.map((mascota) => { mascota.name }) :
+                    <p>Loading Mascotas</p>}
+
+            </div>
+
+        </section>
     )
 }
 
