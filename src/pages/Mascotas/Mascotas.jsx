b88b618a6bea9c21274c5511379cc2c2f6be3980
lisvega <lissyrivera456@gmail.com>
2022-08-28 14:28:51 +0200

    Añadiendo una vez más el .env dentro de git ignore y culminando el último condicional con el buscador

--- src/pages/Mascotas/Mascotas.jsx
+++ src/pages/Mascotas/Mascotas.jsx
@@ -10,12 +10,12 @@ import "./Mascotas.css"
 
 const Mascotas = () => {
     const [allMascotas, setAllMascotas] = useState([]);
-    const [filterWord,setFilterWord] = useState("");
-    
+    const [filterWord, setFilterWord] = useState("");
+
     const setFilteredMascotas = allMascotas.filter((mascota) =>
-    mascota.petname.toLowerCase().includes(filterWord) || 
-    mascota.type.toLowerCase().includes(filterWord) || 
-    mascota.nickname.toLowerCase().includes(filterWord)
+        mascota.petname.toLowerCase().includes(filterWord) ||
+        mascota.type.toLowerCase().includes(filterWord) ||
+        mascota.nickname.toLowerCase().includes(filterWord)
 
     )
 
@@ -33,22 +33,22 @@ const Mascotas = () => {
 
 
     return (
-          <section className="main">
-            
-             <h2>Mascotas</h2>
-         <Search setFilterWord = {setFilterWord} />
+        <section className="main">
 
-       
+            <h2>Mascotas</h2>
+            <Search setFilterWord={setFilterWord} />
+
+
             <div className="gallery">
 
 
-        {allMascotas.length?setFilteredMascotas.map((mascota)=><MascotaCard mascota={mascota} key={mascota._id}/>):<p>Loading Mascota... </p>}
-           
-        </div>
-           
-      
+                {allMascotas.length ? setFilteredMascotas.map((mascota) => <MascotaCard mascota={mascota} key={mascota._id} />) : <p>Loading Mascota... </p>}
 
-        </section> 
+            </div>
+            {!allMascotas.length ? <p>Mascota not found</p> : <p>null</p>}
+
+
+        </section>
     )
 }
 
