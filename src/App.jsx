f281782ca73e837aad4fe064cc6241fca7a978e5
Ramon Williams <ramnwor21@gmail.com>
2022-08-30 10:46:44 +0200

    Dark Mode completo

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,26 @@ import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import { JwtContextProvider } from "./context/jwtContext";
 import RequireAuth from "./components/RequireAuth/RequireAuth";
+import useLocalStorage from 'use-local-storage'
 
 const App = () => {
-  
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? '🌙' : '☀');
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  }
   
   return (
     <JwtContextProvider>
-      <div className="App">
-        
+      <div className="App" data-theme={theme}>
+      <button className="mode" onClick={switchTheme}>Mode {theme === '☀' ? '🌙' : '☀'} </button>
 
         <Router>
+
           <Header />
+         
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/mascotas" element={<Mascotas />} />            
@@ -28,11 +37,8 @@ const App = () => {
             <Route path="/newmascota" element={<RequireAuth><NewMascota /></RequireAuth>} />
             <Route path="/editmascota" element={<RequireAuth> <EditMascota /> </RequireAuth>} />
             <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-
-
-
-          </Routes>
+            <Route path="/login" element={<Login />} />             
+           </Routes>
 
 
           <Footer />
